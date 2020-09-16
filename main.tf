04b2e78e07468e8f6371308cb28d4856edbc86f4
anusha-krishnan <anusha.krishnan@in.ibm.com>
2020-09-16 22:13:19 +0530

    remove vars from main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,4 @@
-variable "ibmcloud_api_key" {
-  description = "Enter your IBM Cloud API Key, you can get your IBM Cloud API key using: https://cloud.ibm.com/iam#/apikeys"
-}
-variable "region" {
-  description = "Region in which vpc resources are to be provisioned"
-}
+
 provider "ibm" {
   ibmcloud_api_key      = var.ibmcloud_api_key
   generation            = 2
